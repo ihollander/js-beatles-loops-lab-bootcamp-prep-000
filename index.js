75df84e06bdf638d5943621c0b6ca8542f448c0c
Ian Hollander <ianholla@gmail.com>
2018-08-03 21:37:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
 function theBeatlesPlay(musicians, instruments) {
-  var newArray = [];
+  var newArray = []
   for (let i = 0; i < musicians.length; i++) {
-    newArray.push(musicians[i] + ' plays ' + instruments[i]);
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`)
   }
-  return newArray;
-}
-
-function johnLennonFacts(facts) {
-  var i = 0;
-  while (i < facts.length) {
-    facts[i] += '!!!';
-    i++;
-  }
-  return facts
-}
-
-function iLoveTheBeatles(number) {
-  var newArray = [];
-  do {
-    newArray.push("I love the Beatles!");
-    number++
-  } while (number < 15)
-  return newArray;
+  return newArray
 }
